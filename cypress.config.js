5a9bb38c0d2d7c80cf56ff38699d367e8cc6989a
Shawn Harris <shawn.harris3510@gmail.com>
2022-12-02 00:34:03 -0500

    trying to use task in config

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,35 @@
 const { defineConfig } = require('cypress')
+const DownloadAndExtractGraphqlInspector = require('./cypress/gqlExtensionDownloader');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-        on('before:browser:launch', (browser, launchOptions) => {
-          
+      // implement node event listeners here
+      on('task', {
+        downloadExtension() {
+          DownloadAndExtractGraphqlInspector();
+        },
+      })
+      on('before:browser:launch', async (browser, launchOptions) => {
+
+        // I want to invoke a task that downloads a chrome extension (if it doesn't already exist),
+        // then add it to the browser configuration
+        cy.task('downloadExtension', { timeout: 1000 }).then((finished) => {
+          // We don't make it to this console log because "cy is undefined" on the above line
+          console.log(finished);
           launchOptions.extensions.push(
-            '/Users/shawn/Desktop/path/to/extension'
+            '/Users/shawn/Desktop/Cypress/customer/cyNotRecognized/cypress-undefined/cypress/gqlExtensionDownloader'
           );
-          return launchOptions
-        })
-      }
+        });
+      });
+    },
   }
 })
+
+// gqlExtensionDownloader.js
+async function DownloadAndExtractGraphqlInspector() {
+  console.log('download an extension and unzip it');
+  return true;
+}
+
+module.exports = DownloadAndExtractGraphqlInspector;
